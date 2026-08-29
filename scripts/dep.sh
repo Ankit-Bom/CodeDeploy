143da9c91566cd02cbfd850c43bef8b3a138afeb
Ankit <162279577+Ankit-Bom@users.noreply.github.com>
2026-08-29 14:03:05 +0530

    Update dep.sh

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-apt-get update -y
-apt-get install -y apache2
-
-rm -f /var/www/html/index.html
-
-systemctl enable apache2
-systemctl start apache2
+sudo apt-get install -y apache2
+rm /var/www/html/index.html
+service apache2 start
